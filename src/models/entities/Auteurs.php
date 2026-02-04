d1791046066b547486148b7f9387bf785db74e89
Erynill <242376261+Erynill@users.noreply.github.com>
2026-02-04 15:13:19 +0100

    Ajout classe auteur + user

---
+++ src/models/entities/Auteurs.php
@@ -0,0 +1,102 @@
+<?php
+declare (strict_types = 1);
+
+namespace App\models\entities;
+
+use Exception;
+
+class Auteurs
+{
+  private ?int $id = null;
+  private string $nom = "";
+  private string $prenom = "";
+  private ?string $biographie = null;
+
+  /**
+   * Get the value of id
+   */
+  public function getId(): int
+  {
+    return $this->id;
+  }
+
+  /**
+   * Set the value of id
+   *
+   * @return  self
+   */
+  public function setId(int $id): self
+  {
+    $this->id = $id;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of nom
+   */
+  public function getNom(): string
+  {
+    return $this->nom;
+  }
+
+  /**
+   * Set the value of nom
+   *
+   * @return  self
+   */
+  public function setNom(string $nom): self
+  {
+    if (strlen($nom) > 255) {
+      throw new Exception("Le nom doit faire au maxium 255 caractères");
+    } else {
+      $this->nom = $nom;
+    }
+
+    return $this;
+  }
+
+  /**
+   * Get the value of prenom
+   */
+  public function getPrenom(): string
+  {
+    return $this->prenom;
+  }
+
+  /**
+   * Set the value of prenom
+   *
+   * @return  self
+   */
+  public function setPrenom(string $prenom): self
+  {
+    if (strlen($prenom) > 255) {
+      throw new Exception("Le prénom doit faire au maximum 255 caractères");
+    } else {
+      $this->prenom = $prenom;
+    }
+
+    return $this;
+  }
+
+  /**
+   * Get the value of biographie
+   */
+  public function getBiographie(): string
+  {
+    return $this->biographie;
+  }
+
+  /**
+   * Set the value of biographie
+   *
+   * @return  self
+   */
+  public function setBiographie(string $biographie): self
+  {
+    $this->biographie = $biographie;
+
+    return $this;
+  }
+}
---
+++ src/models/entities/User.php
@@ -0,0 +1,120 @@
+<?php
+declare (strict_types = 1);
+
+namespace App\models\entities;
+
+use DateTime;
+use Exception;
+
+class User
+{
+  private ?int $id = null;
+  private string $username = "";
+  private string $password = "";
+  private string $role = "";
+  private ?DateTime $created_at = null;
+
+  /**
+   * Get the value of id
+   */
+  public function getId(): int
+  {
+    return $this->id;
+  }
+
+  /**
+   * Set the value of id
+   *
+   * @return  self
+   */
+  public function setId(int $id): self
+  {
+    $this->id = $id;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of username
+   */
+  public function getUsername(): string
+  {
+    return $this->username;
+  }
+
+  /**
+   * Set the value of username
+   *
+   * @return  self
+   */
+  public function setUsername(string $username): self
+  {
+    $this->username = $username;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of password
+   */
+  public function getPassword(): string
+  {
+    return $this->password;
+  }
+
+  /**
+   * Set the value of password
+   *
+   * @return  self
+   */
+  public function setPassword(string $password): self
+  {
+    $this->password = password_hash($password, PASSWORD_DEFAULT);
+
+    return $this;
+  }
+
+  /**
+   * Get the value of role
+   */
+  public function getRole(): string
+  {
+    return $this->role;
+  }
+
+  /**
+   * Set the value of role
+   *
+   * @return  self
+   */
+  public function setRole(string $role): self
+  {
+    if (!$role === "user" || !$role === "admin") {
+      throw new Exception("Le rôle ne correspond pas, soit admin ou user");
+    } else {
+      $this->role = $role;
+    }
+
+    return $this;
+  }
+
+  /**
+   * Get the value of created_at
+   */
+  public function getCreated_at(): DateTime
+  {
+    return $this->created_at;
+  }
+
+  /**
+   * Set the value of created_at
+   *
+   * @return  self
+   */
+  public function setCreated_at(DateTime $created_at): self
+  {
+    $this->created_at = $created_at;
+
+    return $this;
+  }
+}
