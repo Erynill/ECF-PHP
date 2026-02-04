f97e8d015014200b8b5816fa338b76459bb359ec
Erynill <242376261+Erynill@users.noreply.github.com>
2026-02-04 09:16:56 +0100

    Initialisaton

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+use Dotenv\Dotenv;
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+require dirname(__DIR__) . "/config/router.php";
