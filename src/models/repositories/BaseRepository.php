bc4d0f80d6ec90f36bf88a7405d5d611a8d727d7
Erynill <242376261+Erynill@users.noreply.github.com>
2026-02-04 11:10:19 +0100

    Classe abstraite repository

---
+++ src/models/repositories/BaseRepository.php
@@ -0,0 +1,27 @@
+<?php
+declare (strict_types = 1);
+
+namespace App\models\repositories;
+
+use App\models\core\Database;
+use PDO;
+
+abstract class BaseRepository
+{
+  protected PDO $db;
+
+  public function __construct()
+  {
+    $this->db = Database::getInstance()->getConnexion();
+  }
+
+  abstract public function create(object $data): bool;
+
+  abstract public function getAll(): array;
+
+  abstract public function getById(int $id): ?object;
+
+  abstract public function updateById(int $id): bool;
+
+  abstract public function deleteById(int $id): bool;
+}
